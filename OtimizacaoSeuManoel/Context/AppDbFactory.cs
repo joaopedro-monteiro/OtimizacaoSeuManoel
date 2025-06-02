f49ecbbb946a9672b978f10babc83c54b2716c66
João Pedro <Joo Pedro@DESKTOP-CKGL72I>
2025-06-02 01:00:21 -0300

    Alterando a string de conexão para incluir o docker

--- OtimizacaoSeuManoel/Context/AppDbFactory.cs
+++ OtimizacaoSeuManoel/Context/AppDbFactory.cs
@@ -7,7 +7,7 @@ public class AppDbFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
-        const string connectionString = "Server=localhost;Database=SeuManoel;Integrated Security=True;TrustServerCertificate=True;";
+        const string connectionString = "Server=localhost;Database=SeuManoel;User Id=sa;Password=ef66b58b-6ff2-4c78-bcec-6b279312b625;TrustServerCertificate=True;";
 
         var construtor = new DbContextOptionsBuilder<AppDbContext>();
         construtor.UseSqlServer(connectionString);
